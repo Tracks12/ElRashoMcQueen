a7372bae5987abd07be5840f88c4a20091e817a3
Mirra Mathias <52259162+Mirra-Mathias@users.noreply.github.com>
2022-05-03 10:57:48 +0200

    add onClick menu

--- Assets/Behaviors/buttonPlay.cs
+++ Assets/Behaviors/buttonPlay.cs
@@ -1,23 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class buttonPlay : MonoBehaviour
 {
-    public Button yourButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Button btn = yourButton.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
-    }
 
-    // Update is called once per frame
-    void TaskOnClick()
+   public void MoveToScene(int sceneID)
     {
-        Debug.Log("You have clicked the button!");
-        SceneManager.LoadScene("Racetracks", LoadSceneMode.Additive);
+        SceneManager.LoadScene(sceneID);
     }
+    
 }
